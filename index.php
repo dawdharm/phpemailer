5ce3d173a7c063fb68de8a9b940f1b8b1aa17f53
Dawn Dharmishtan <114485736+dawdharm@users.noreply.github.com>
2026-03-13 00:22:23 +0530

    mail to www

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-header("Location: https://www.".$_SERVER["SERVER_NAME"]."/");
+$host = preg_replace('/^mail\./i', '', $_SERVER["SERVER_NAME"]);
+header("Location: https://www.".$host."/");
 die();
 
 ?>
